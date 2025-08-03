72691910b6f37ae17277a387ace52d8071e238da
rrlerr <rrlerr60@gmail.com>
2025-08-03 17:58:57 +0530

    Added Button Functionality In App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
 import ProductManager from './pages/ProductManager';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
