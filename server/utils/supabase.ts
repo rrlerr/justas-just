c4448a11f5b925489dfa9e37646051af33459b12
rrlerr <rrlerr60@gmail.com>
2025-08-04 22:08:52 +0530

    Update supabase.ts

--- server/utils/supabase.ts
+++ server/utils/supabase.ts
@@ -1,9 +1,8 @@
-// server/utils/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY! // You must set this key in .env
 );
 
 export default supabase;
